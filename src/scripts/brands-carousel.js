58860aabec2487e4dcd7a18a432aa191f6030b5e
devjson <dev.json.stringify@gmail.com>
2021-11-14 19:11:11 +0800

    Task 2: Refract:  brands-carousel.js remove jQuery dep. Update: brands-carousel Markup structure Add: splide as carousel dep. (no jQuery)

--- src/scripts/brands-carousel.js
+++ src/scripts/brands-carousel.js
@@ -1,19 +1,20 @@
-import $ from "jquery";
-
+import Splide from '@splidejs/splide';
 
 const carouselSpeed = 1500;
 
-$(document).ready(function(){
-    $('#brands-carousel').slick({
-        slidesToShow: 1,
-        infinite: true,
-        speed: carouselSpeed,
+document.addEventListener( 'DOMContentLoaded', function () {
+    const splide = new Splide( '#brands-carousel', {
         autoplay: true,
-        autoplaySpeed: 3000,
-        cssEase: 'cubic-bezier(0.76, 0, 0.24, 1)',
-
         arrows: false,
-        dots: true,
-        appendDots: $('#brands-carousel-dots'),
+        perPage: 1,
+        type   : 'slide',
+        speed: 3000,
+        easing: 'cubic-bezier(0.76, 0, 0.24, 1)',
     });
-});
+
+    splide.on( 'pagination:mounted', function ( data ) {
+        data.list.classList.add( 'splide__pagination__brands-carousel' );
+    });
+
+    splide.mount();
+  });
