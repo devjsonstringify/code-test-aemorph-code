1ea79958855577881f846418d7aeebf2bf1f7caf
devjson <dev.json.stringify@gmail.com>
2021-11-14 22:22:55 +0800

    Refract:  smooth-scroll-links.js removed jQuery dep.

--- src/scripts/smooth-scroll-links.js
+++ src/scripts/smooth-scroll-links.js
@@ -1,18 +1,5 @@
-import $ from "jquery";
+const links = document.querySelectorAll(".js-smooth-scroll");
 
-
-$('.js-smooth-scroll').click( function(event){
-    
-    const href = $(this).attr('href');
-    const targetId = href.substring(href.indexOf("#"));
-    const targetEl = $(targetId);
-
-    if(targetEl.length){
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: targetEl.offset().top
-        }, 1000);
-        location.hash = targetId;
-    }
-
+links.forEach((link) => {
+    link.scrollIntoView({ behavior: 'smooth', block: 'center',});
 });
