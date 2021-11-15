3fd9fad823c5ae8ed3a0251d74c3b0868ded705c
devjson <dev.json.stringify@gmail.com>
2021-11-16 07:54:09 +0800

    Task 2: Refract: events-carousel no jQuery dependency

--- src/scripts/events-carousel.js
+++ src/scripts/events-carousel.js
@@ -1,24 +1,28 @@
-import $ from "jquery";
+import Splide from '@splidejs/splide';
 
 const carouselSpeed = 900;
-
-$(document).ready(function(){
-    $('#events-carousel').slick({
-        slidesToShow: 1,
-        infinite: true,
+document.addEventListener( 'DOMContentLoaded', function () {
+    const splideEventsCarousel = new Splide( '.events-carousel-wrap', {
+        autoplay: false,
+        arrows: false,
+        perPage: 2,
+        pagination: false,
+        type   : 'loop',
         speed: carouselSpeed,
-        cssEase: 'cubic-bezier(0.76, 0, 0.24, 1)',
-
-        arrows: true,
-        appendArrows: $('#events-arrows'),
-
-        mobileFirst: true,
-        responsive: [{
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-            }
-        }],
-
+        easing: 'cubic-bezier(0.76, 0, 0.24, 1)',
+        mediaQuery: 'max',
+        breakpoints: {
+            992: {
+                perPage: 1,
+            },
+      }
     });
-});
+   
+    const nextBtn = document.querySelector('.events-carousel-next');
+    const prevBtn = document.querySelector('.events-carousel-prev');
+
+    // Manually added control buttons outside splide required markup structure
+    nextBtn.addEventListener('click', () => splideEventsCarousel.go( '>' ));
+    prevBtn.addEventListener('click', () => splideEventsCarousel.go( '<' ));
+    splideEventsCarousel.mount();
+  });
